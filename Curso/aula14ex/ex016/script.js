2024bacdf8bbd1bff5df98059229a48a2043b71e
ViniciusdeASantana <148257713+ViniciusdeASantana@users.noreply.github.com>
2025-02-13 18:33:18 -0300

    corrigindo o exercício 14 local errado

---
+++ Curso/aula14ex/ex016/script.js
@@ -0,0 +1,8 @@
+function somar() {
+    var inicio = window.document.getElementsByClassName('number__box')
+    var fim = window.document.getElementsByClassName('number__box-2')
+    var passo = window.document.getElementsByClassName('number__box-3')
+    var s1 = Number(inicio.value)
+    var s2 = Number(fim.value)
+    var number__count = s1 + s2
+}
